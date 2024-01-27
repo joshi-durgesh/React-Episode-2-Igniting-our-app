081824536a1a8f280d6cb5246dd3ec816872a8aa
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-01-27 11:32:29 +0530

    created element using javascript

---
+++ app.js
@@ -0,0 +1,6 @@
+//normal javascript
+const container = document.getElementById("container");
+container.innerHTML = `
+<h1>This heading is created with JavaScript 🍷</h1>
+<p>This is the paragraph created with JavaScript</p>
+`;
