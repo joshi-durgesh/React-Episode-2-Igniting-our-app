a51ba94a23339b3e9e727246914346dbea78a8f7
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-01-27 12:17:14 +0530

    creating element through cdn react

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-//normal javascript
-const container = document.getElementById("container");
-container.innerHTML = `
-<h1>This heading is created with JavaScript 🍷</h1>
-<p>This is the paragraph created with JavaScript</p>
-`;
+//using react
+//
+//creating element
+const content = React.createElement("div", {}, [
+  React.createElement(
+    "h1",
+    {},
+    "This heading is created with React through CDN"
+  ),
+  React.createElement("p", {}, "this paragraph is created with react"),
+]);
+
+//creating root
+const root = ReactDOM.createRoot(document.getElementById("container"));
+
+//rendering our element in root
+root.render(content);
